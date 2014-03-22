716af7c5d2cad318ef14d13677673d9e883896a2
Brian McQuay <brian@onomojo.com>
2014-03-21 21:05:22 -0600

    adding scrolling background

---
+++ Assets/Scripts/Scroller.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var speed : float;
+var xBound : int;
+
+function Update () {
+  if(transform.position.x <= xBound) {
+    transform.position = new Vector3 (transform.position.x + 1 * speed, transform.position.y, transform.position.z);
+  } else {
+    transform.position = new Vector3 (-xBound + 1 * speed, transform.position.y, transform.position.z);
+  }
+}
