7853e4b61f091cc3d2afae1a91e5fbd1d13d0d25
Travis Palmer <travis@cramerdev.com>
2014-03-26 21:26:13 -0400

    first draft of boosting

---
+++ Assets/Scripts/Boost.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var boost : Vector3;
+var home : Vector3;
+var boosting : boolean;
+
+function Start () {
+	home = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+}
+
+function Update () {
+	if (!boosting) {
+		var currentPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		var rb = GetComponent.<Rigidbody>();
+		
+		if (currentPosition.x < home.x) {
+			// Vehicle is to the right of the starting position.
+			rb.velocity = Vector3(3, 0, 0);	
+		}
+		else {
+			// Vehicle is to the left of the starting position.
+			rb.velocity = Vector3(0, 0, 0);
+		}
+	}
+}
+
+function OnMouseDown () {
+	boosting = true;
+	
+	var rb = GetComponent.<Rigidbody>();
+	rb.velocity = boost;
+}
+
+function OnMouseUp () {
+	boosting = false;
+	
+	var rb = GetComponent.<Rigidbody>();
+	rb.velocity = Vector3(0, 0, 0);
+}
